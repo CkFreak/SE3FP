a2135dc8e08551c1722bf5932160b2f4b7c99d60
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-10-26 13:58:34 +0200

    Bogenmass zu grad gebaut

--- Hausaufgaben1.rkt
+++ Hausaufgaben1.rkt
@@ -2,6 +2,8 @@
 ; grad * 2 * pi /360
 (define (gradZuBogenmass grad)
   (/ (* grad 2 pi) 360))
-;360/2*pi +bogenmaß
-(define bogenmassZuGrad bogen)
-()
+
+
+;360/2*pi * bogenmaß
+(define (bogenmassZuGrad bogen)
+(* (/ 360 (* 2 pi)) bogen))

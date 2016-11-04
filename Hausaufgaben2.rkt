acaf9bb807000166c469105169da8599f0081511
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-11-04 12:21:00 +0100

    Aufgabe eins Fertig

--- Hausaufgaben2.rkt
+++ Hausaufgaben2.rkt
@@ -1,6 +1,8 @@
 #lang racket
 ;Hausaufgaben 2
 
+#|Aufgabe 1 Symbole und Werte, Umgebungen|#
+
 (define miau 'Plueschi)
 (define katze miau)
 (define tiger 'miau)
@@ -19,12 +21,16 @@
 2. katze --> 'Plueschi (durch katze wird miau aufgerufen)
 3. tiger --> 'miau ('miau ist hier ein symbol und keine Funktion)
 4. (quote katze) --> (gleiche Auswrkung wie ' --> Eingabe wird zum symbol)
-5. (eval tiger) -->
-6. (eval katze) -->
-7. (eval 'tiger) -->
+5. (eval tiger) --> 'Plueschi, weil eval die Quote auf 'miau ingnoriert und die funktion auswertet
+6. (eval katze) -->  Fehler, weil eval versucht, 'Plueschi auszuführen, diese Funktion aber nicht definiert ist
+7. (eval 'tiger) --> 'miau, da die evaluierung von 'tiger auf 'miau zeigt. eval zeigt hier die paramteter von tiger an.
 8. (welcherNameGiltWo 'harry 'potter) --> 'harry (Ausdruecke werden nicht nacheinander ausgewertet)
 9. (cdddr xs1) --> '(miau katze) (Liste wird ohne erste 3 Elemente ausgegeben (3 x d))
 10 (cdr xs2) --> '(Plueschi) (Liste Plueschi Plueschi ohne 1. Element)
 11. (cdr xs3) --> '(Plueschi . Plueschi) (cons fuegt 2 Elemente zu einem Paar zusammen)
-12. (eval (sqrt 3)) -->
+12. (eval (sqrt 3)) --> 1.7320508075688772, da es hier keine Quote gibt und so sqrt einfach ausgewertet wird
+13. (eval ’(welcherNameGiltWo ’tiger ’katze)) --> 'tiger, weil welcherNameGiltWo ausgewertet wird und PersonA immer ausgegeben wird--> 'tiger
+14. (eval (welcherNameGiltWo ’katze ’tiger )) --> 'miau, weil tiger ausgewertet wird
+
+eval ignoriert die erste Quote die es findet und wertet dem Ausdruck aus
 |#

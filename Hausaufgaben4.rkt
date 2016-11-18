cf5e732626eead223ae5818033a371cb918e5fca
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-11-18 13:49:11 +0100

    Aufgabe 1 fertig

--- Hausaufgaben4.rkt
+++ Hausaufgaben4.rkt
@@ -1,1 +1,40 @@
 #lang racket
+#|
+Softwareentwicklung 3 Blatt 4
+
+Aufgabe 1 Auswertung von Ausdrücken
+|#
+  
+(max(min 5 (- 6 7)) 8)
+
+'(+ (- 11 13) 17)
+
+(cadr '(Alle Jahre wieder))
+
+(cddr '(kommt (das Christuskind)))
+
+(cons 'Auf'(die Erde nieder))
+
+(cons 'Wo 'wir)
+
+(equal? (list 'Menschen 'sind) '(Menschen sind))
+
+(eq? (list 'Rudolph 'Das 'Rentier) (cons 'Rudolph '(Das Rentier)))
+
+#|
+(max(min 5 (- 6 7)) 8) -> 8
+
+'(+ (- 11 13) 17) -> '(+ (- 11 13) 17)
+
+(cadr '(Alle Jahre wieder)) -> 'Jahre
+
+(cddr '(kommt (das Christuskind))) -> '()
+
+(cons 'Auf'(die Erde nieder)) -> '(Auf die Erde nieder)
+
+(cons 'Wo 'wir) -> '(Wo . wir)
+
+(equal? (list 'Menschen 'sind) '(Menschen sind)) -> #t
+
+(eq? (list 'Rudolph 'Das 'Rentier) (cons 'Rudolph '(Das Rentier))) -> #f
+|#

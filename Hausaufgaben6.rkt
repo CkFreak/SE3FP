77ee38155b7479ad60ffca90c9619da7a0866975
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-09 13:31:59 +0100

    Funktion zum Baum malen gebaut, triangleList muss irgendwie noch komplett gedreht werden

--- Hausaufgaben6.rkt
+++ Hausaufgaben6.rkt
@@ -2,8 +2,15 @@
 #|Hausaufgaben Blatt 06|#
 (require 2htdp/image)
 
+;;Generiert eine Liste aus Dreiecken, die mit jedem Dreieck um 1 in der Länge reduziert werden.
 (define (generateTriangles baseSize)
-  (if (< baseSize 3)
+  (if (< baseSize 10)
       '()
-      (cons (triangle baseSize "solid" "green") (generateTriangles (- baseSize 1)))))
+      (cons (triangle baseSize "solid" "green") (generateTriangles (- baseSize 5)))))
+
+
+(define (drawTree triangleList)
+  (if (= (length triangleList) 2)
+      (overlay/offset (car triangleList) 0 10 (cadr triangleList))
+      (overlay/offset (car triangleList) 0 10 (drawTree (cdr triangleList)))))
       

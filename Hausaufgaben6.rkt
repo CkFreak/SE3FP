ff3af53864515635db174b4837d4effced68ed68
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-09 13:33:44 +0100

    Kommentare eingefügt

--- Hausaufgaben6.rkt
+++ Hausaufgaben6.rkt
@@ -2,13 +2,15 @@
 #|Hausaufgaben Blatt 06|#
 (require 2htdp/image)
 
+#|Aufgabe 2 Nikolausaufgabe|#
+
 ;;Generiert eine Liste aus Dreiecken, die mit jedem Dreieck um 1 in der Länge reduziert werden.
 (define (generateTriangles baseSize)
   (if (< baseSize 10)
       '()
       (cons (triangle baseSize "solid" "green") (generateTriangles (- baseSize 5)))))
 
-
+;;Malt aus einer Liste an Dreiecken einen Tannenbaum
 (define (drawTree triangleList)
   (if (= (length triangleList) 2)
       (overlay/offset (car triangleList) 0 10 (cadr triangleList))

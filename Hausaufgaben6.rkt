dd8a915035d74fab605224cdb4b1b6eb745d56db
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-09 13:01:54 +0100

    Funktion zum erstellen von grünen Dreiecken gebaut

--- Hausaufgaben6.rkt
+++ Hausaufgaben6.rkt
@@ -1,2 +1,9 @@
 #lang racket
 #|Hausaufgaben Blatt 06|#
+(require 2htdp/image)
+
+(define (generateTriangles baseSize)
+  (if (< baseSize 3)
+      '()
+      (cons (triangle baseSize "solid" "green") (generateTriangles (- baseSize 1)))))
+      

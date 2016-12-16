693f5b65bf7391b5c2bff1b1984215bbac6ef9aa
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-16 12:48:54 +0100

    2.1 eingefügt

--- Hausaufgaben7.rkt
+++ Hausaufgaben7.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+#|Aufgabe 1 Produktberechnung mit drei verschienenen Methoden|#
 ;;Errechnet das Produkt allgemein Rekursiv
 (define (produkt n f)
   (if (null? n)
@@ -13,5 +13,25 @@
       (reverse el)
       (produktEndRekursiv (cdr n) f (cons (* (car n) f) el))))
 
+;;Errechnet das Produkt mit Hilfe von Funktionen Höherer Ordnung
 (define (produktHigh n f)
   (map ((curry *) f) n))
+
+#|Aufgabe 2 Die Sieben-Segment Anzeige|#
+
+;;2.1 Kodierung der 7-Segment Anzeige Zustände
+;;Eine 0 steht für inanktiv und eine 1 für aktiv
+;; der oberere Querbalken ist position 0 und der untere querbalken position 6
+;;es wird von links nach rechts durchnummeriert
+
+(define anzeigeCodes'((0 0 0 0 0 0 0) ;;0
+                       (0 0 1 0 0 1 0) ;;1
+                       (1 0 1 1 1 0 1) ;;2
+                       (1 0 1 1 0 1 1) ;;3
+                       (0 1 1 1 0 1 0) ;;4
+                       (1 1 0 1 0 1 1) ;;5
+                       (1 1 0 1 1 1 1) ;;6
+                       (1 0 1 0 0 1 0) ;;7
+                       (1 1 1 1 1 1 1) ;;8
+                       (1 1 1 1 0 1 1) ;;9
+                       ))

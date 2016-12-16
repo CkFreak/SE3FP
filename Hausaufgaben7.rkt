a47370bdcbaea8ccb4e77e548506e54b822c72a7
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-16 10:33:56 +0100

    Endrekursives Produkt fertig

--- Hausaufgaben7.rkt
+++ Hausaufgaben7.rkt
@@ -5,3 +5,10 @@
   (if (null? n)
      '()
       (cons (* (car n) f) (produkt (cdr n) f))))
+
+;;Errechnet das Produkt endrekursiv
+;;el ist eine emptyList
+(define (produktEndRekursiv n f el)
+  (if (null? n)
+      (reverse el)
+      (produktEndRekursiv (cdr n) f (cons (* (car n) f) el))))

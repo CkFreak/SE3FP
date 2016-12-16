8569cb8e30c2bcce935148806b6bf71ef98e5142
CkFreak <5blaesche@informatik.uni-hamburg.de>
2016-12-16 13:24:00 +0100

    2.2 angefangen

--- Hausaufgaben7.rkt
+++ Hausaufgaben7.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require 2htdp/image)
+
 #|Aufgabe 1 Produktberechnung mit drei verschienenen Methoden|#
 ;;Errechnet das Produkt allgemein Rekursiv
 (define (produkt n f)
@@ -6,9 +8,12 @@
      '()
       (cons (* (car n) f) (produkt (cdr n) f))))
 
+(define (produktEndRekursiv n f)
+  ((produktEndRekursiv n f '())))
+
 ;;Errechnet das Produkt endrekursiv
 ;;el ist eine emptyList
-(define (produktEndRekursiv n f el)
+(define (produktEndRekursivHilf n f el)
   (if (null? n)
       (reverse el)
       (produktEndRekursiv (cdr n) f (cons (* (car n) f) el))))
@@ -35,3 +40,22 @@
                        (1 1 1 1 1 1 1) ;;8
                        (1 1 1 1 0 1 1) ;;9
                        ))
+
+(define (drawSegment)
+  (overlay/align "middle" "top"
+                 (drawVerticalSegment 1)
+  (overlay/align "middle" "middle"
+               (drawVerticalSegment 1)
+               (rectangle 100 200 "solid" "black"))))
+
+;;0 für grau 1 für Rot
+(define (drawVerticalSegment color)
+  (if (= color 1)
+      (rectangle 80 10 "solid" "Red")
+      (rectangle 80 10 "solid" "DimGray")))
+
+;;
+(define (drawHorizontalSegment color)
+  (if (= color 1)
+      (rectangle 10 80 "solid" "Red")
+      (rectangle 10 80 "solid" "DimGray")))

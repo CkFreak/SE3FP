86d57d6c217698510e241b8d683b03d7252e16e0
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-01-03 16:03:39 +0100

    1.1 korrigiert, 1.2 begonnen

--- Hausaufgaben8.rkt
+++ Hausaufgaben8.rkt
@@ -1,4 +1,16 @@
 #lang racket
 #|Aufgabe 1.1
-Eine Funktion in Racket ist dann eine Funktion höherer Ordnung, wenn sie eine Funktion als Variable entgegennehmen. 
+Eine Funktion in Racket ist dann eine Funktion höherer Ordnung, wenn sie eine Funktion als Variable entgegennehmen oder als Rückgabewert haben. 
+
+Aufgabe 1.2
+a) foldr
+Hierbei handelt es sich um eine Funktion Höherer Ordnung. foldr nimmt eine Zwei Parametrige Funktion als input und einen Basiswert.
+Die Funktion wird dann auf eine Liste mit Werten angewandt.
+
+b) (plus-oder-minus x)
+Diese Funktion gibt aus, ob der eingegebene Wert x positiv oder negativ ist.
+Es wird keine Funktion engegegengenommen. Also handlet es sich hierbei nicht um eine Funktion Höherer Ordnung.
+
+c) 
 |#
+

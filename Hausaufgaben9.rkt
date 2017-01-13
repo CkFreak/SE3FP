af51747dc882260be92f3bf198f7efb84a76fe48
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-01-13 12:40:09 +0100

    Klassen definiert für Literatur

--- Hausaufgaben9.rkt
+++ Hausaufgaben9.rkt
@@ -1,1 +1,28 @@
 #lang swindle
+(require swindle/setf
+         swindle/misc)
+
+(defclass* Literatur ()
+  (items
+   :schluessel
+   :autor
+   :erscheinungsjahr
+   :titel )
+  :printer #t)
+
+(defclass* Schluessel ()
+  (schluessel :accessor schl :initarg :schlu
+              )
+  :printer #t)
+
+(defclass* Autor ()
+  (autor :accessor aut :initarg :auto)
+  :printer #t)
+
+(defclass* Erscheinungsjahr ()
+  (erscheinungsjahr :accessor erschJ :initvalue :jahr)
+  :printer #t)
+
+(defclass Titel ()
+  (titel :accessor tit :initvalue :titel)
+  :printer #t)

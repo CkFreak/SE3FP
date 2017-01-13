a43d603bb98124f18b79a38de7610db348a3b54d
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-01-13 13:14:03 +0100

    Aufgabe 1 besser gelöst

--- Hausaufgaben9.rkt
+++ Hausaufgaben9.rkt
@@ -2,27 +2,46 @@
 (require swindle/setf
          swindle/misc)
 
-(defclass* Schluessel ()
-  (schluessel :accessor schl :initarg :schlu
-              )
+(defclass* Literatur ()
+  (autoren
+   :reader autoren
+   :initarg :aut
+   :type <list>)
+  (jahr
+   :reader jahr
+   :initarg :jr
+   :type <integer>)
+  (titel
+   :reader titel
+   :initarg :tl
+   :type <string>)
+  (schluessel
+   :initarg :schluessel
+   :reader schluessel
+   :type <integer>
+)
   :printer #t)
 
-(defclass* Autor ()
-  (autor :accessor aut :initarg :auto)
-  :printer #t)
+(defclass* Buch (Literatur)
+  (verlag
+   :reader verlag
+   :initarg :verl
+   :type <string>)
+  
+   (verlagsort
+   :reader ort
+   :initarg :ort
+   :type <string>)
 
-(defclass* Erscheinungsjahr ()
-  (erscheinungsjahr :accessor erschJ :initarg :jahr)
-  :printer #t)
+  
+   (reihe
+   :reader reihe
+   :initarg :rh
+   :type <string>)
 
-(defclass Titel ()
-  (titel :accessor tit :initarg :titel)
-  :printer #t)
-
-(defclass* Literatur (Schluessel Autor Erscheinungsjahr Titel)
-  (items
-   :schluessel
-   :autor
-   :erscheinungsjahr
-   :titel )
+  
+   (serienummer
+   :reader seriennr
+   :initarg :snr
+:type <string>)
   :printer #t)

68417c591c733b859a837cf7f6dc825dc6f176e0
KaroNe <karolinney@web.de>
2017-01-22 18:41:14 +0100

    Teil 2 Aufgabe 1

--- Hausaufgaben10.rkt
+++ Hausaufgaben10.rkt
@@ -19,3 +19,25 @@ Ney, Ramburger, Blaesche|#
 (if (= 0 y)
     10
     (* y 9)))
+
+; Wiederholung, Klausurvorbereitung
+
+#| Aufgabe 1
+(a) -1. Zunächst wird 2-3 berechnet, davon und 2 das Minimum gesucht (-1)
+und das Maximum von -1 ist -1.
+(b) '(+ ,(- 2 4) 2). Das Quote am Anfang gibt an, dass alles in der
+Klammer eine Liste ist.
+(c) 'Alle. Mit car greift man auf das 1. Element einer Liste zu.
+(d) '((dem See)). Mit cdr greift man auf den Rest einer Liste ohne das 1.
+Element zu.
+(e) '((Listen) sind einfach). cons fügt die 2 Listen zu einer
+Liste zusammen.
+(f) '(Paare . auch). cons fügt die 2 Elemente zu einem Paar zusammen.
+(g) #t. list und ' sind äquivalente Listennoationen.
+(h) #f. Da die beiden Listen zwar gleich aussehen, aber auf unterschiedliche
+Objekte verweisen.
+(i) '(3 6 9). map wendet die lamda Funktion auf jedes Element der Liste an.
+(j) '(2 4). filter filtert die Liste nach der Bedingung even?.
+(k) 2.
+(l) #t.
+|#
